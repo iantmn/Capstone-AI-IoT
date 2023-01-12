4d78900ae0e5b887609d2a8a80c858ded801d875
Gijs Volkers <g.l.volkers@student.tudelft.nl>
2023-01-12 09:08:53 +0100

    alles van Stofzuigen

--- GoPro/GoPro_Telemetry_Code.js
+++ GoPro/GoPro_Telemetry_Code.js
@@ -4,13 +4,13 @@ const gpmfExtract = require('gpmf-extract');
 const goproTelemetry = require(`gopro-telemetry`);
 const fs = require('fs');
 
-const file = fs.readFileSync('D:/DCIM/100GOPRO/GH010018.mp4');
+const file = fs.readFileSync('C:/Users/gijsl/Downloads/Ian_stofzuigen/voorkamer_tapijt.mp4');
 
 
 gpmfExtract(file)
   .then(extracted => {
     goproTelemetry(extracted, {}, telemetry => {
-      fs.writeFileSync('output_path_test2.json', JSON.stringify(telemetry));
+      fs.writeFileSync('output_path_voorkamer_tapijt.json', JSON.stringify(telemetry));
       console.log('Telemetry saved as JSON');
     });
   })
--- GoPro/jsontocsv.py
+++ GoPro/jsontocsv.py
@@ -2,37 +2,54 @@ import json
 import pandas as pd
 from datetime import datetime
 
+all_paths = [   "GoPro/Ian_stofzuigen/Json_files/output_path_gang_langestrook_2",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_keuken_langestrook",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_slaapkamer_alles",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_slaapkamer_onder_bed",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_gang_langestrook",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_trap_2",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_slaapkamer_langestrook",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_trap",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_voorkamer_bank",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_voorkamer_lange_stroken",
+                "GoPro/Ian_stofzuigen/Json_files/output_path_voorkamer_tapijt"
+                ]
+desired_data = "ACCL" # can also be "GYRO"
 
-with open('output_path_test1.json', encoding='utf-8') as inputfile:
-    df = json.load(inputfile)
-    df = df["1"]["streams"]["GYRO"]["samples"]
 
-    lst: list[list[str | float]] = []
-    lst.append([])
-    dct = df[0]
-    time0 = dct["date"]
-    time0 = time0[11:-1]
+for path in all_paths:
 
-    time0 = datetime.strptime(time0, '%H:%M:%S.%f')
-    lst[0].append((time0-time0).total_seconds())
-    for j in range(len(dct["value"])):
-        lst[0].append(dct["value"][j])
 
-    for i in range(1, len(df)):
-        dct = df[i]
+    with open(f'{path}.json', encoding='utf-8') as inputfile:
+        df = json.load(inputfile)
+        df = df["1"]["streams"][desired_data]["samples"]
+
+        lst: list[list[str | float]] = []
         lst.append([])
-        time = dct["date"]
-        time = time[11:-1]
-        time = datetime.strptime(time, '%H:%M:%S.%f')
-        lst[i].append((time-time0).total_seconds())
-        for j in range(len(dct["value"])):
-            lst[i].append(dct["value"][j])
+        dct = df[0]
+        time0 = dct["date"]
+        time0 = time0[11:-1]
 
-with open('csvfile_test1_gyro.csv', 'w') as f:
-    for i in range(len(lst)):
-        for j in range(len(lst[i])):
-            f.write(f"{lst[i][j]}")
-            if j + 1 < len(lst[i]):
-                f.write(f',')
-            else:
-                f.write('\n')
+        time0 = datetime.strptime(time0, '%H:%M:%S.%f')
+        lst[0].append((time0-time0).total_seconds())
+        for j in range(len(dct["value"])):
+            lst[0].append(dct["value"][j])
+
+        for i in range(1, len(df)):
+            dct = df[i]
+            lst.append([])
+            time = dct["date"]
+            time = time[11:-1]
+            time = datetime.strptime(time, '%H:%M:%S.%f')
+            lst[i].append((time-time0).total_seconds())
+            for j in range(len(dct["value"])):
+                lst[i].append(dct["value"][j])
+
+    with open(f'{path}-{desired_data}.csv', 'w') as f:
+        for i in range(len(lst)):
+            for j in range(len(lst[i])):
+                f.write(f"{lst[i][j]}")
+                if j + 1 < len(lst[i]):
+                    f.write(f',')
+                else:
+                    f.write('\n')
